c701c3ecb319bee5a0b2e929538e0dbcb8f466e2
brvasya <crazygames888@gmail.com>
2026-01-21 12:23:56 +0300

    add

--- page.php
+++ page.php
@@ -13,6 +13,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'page
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
 <link rel="stylesheet" href="/style.css">
 <script defer src="/colors.js"></script>
+<script async src="https://cse.google.com/cse.js?cx=d0a39b24af4ab40a8"></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6180203036822393" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-F24J0X7PDM"></script>
 <script>
@@ -20,17 +21,6 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-F24J0X7PDM');
-</script>
-<script>
-(function() {
-  var cx = 'partner-pub-6180203036822393:4728109199';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
 </script>
 </head>
 
