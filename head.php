456a45675a2b0b4d9076729f2d9bf526cb355fba
brvasya <crazygames888@gmail.com>
2026-05-19 12:16:00 +0300

    Update head.php

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="/style.css">
 <script defer src="/colors.js"></script>
 <script async src="https://cse.google.com/cse.js?cx=d0a39b24af4ab40a8"></script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6180203036822393" crossorigin="anonymous"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4677496585017452" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-F24J0X7PDM"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
