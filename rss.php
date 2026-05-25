1bf8423ffb5a5ef577a68a771c26b0f5b2455b13
brvasya <crazygames888@gmail.com>
2026-05-25 19:50:31 +0300

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'app/index_pre.php';
-$pages = array_slice($pages, 0, 50);
+$items = (int) $_GET['items'];
+$pages = array_slice($pages, 0, $items);
 header('Content-Type: application/rss+xml; charset=utf-8');
 echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 ?>
