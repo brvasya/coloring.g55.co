8abcf06f88088061d2cd33b2dc78d4ff6fca57f2
brvasya <crazygames888@gmail.com>
2026-06-30 02:06:45 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <nav>
 <h2>Explore All Coloring Page Categories</h2>
 <ul class="categories">
-<?php foreach ($categories as $c): ?>
+<?php foreach ($grouped['clusters'] as $cluster): $c = $cluster[0]; ?>
 <li><a class="tag" href="/?c=<?php echo rawurlencode($c['id']); ?>"><?php echo h($c['name']); ?></a></li>
 <?php endforeach; ?>
 </ul>
